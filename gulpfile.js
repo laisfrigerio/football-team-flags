d82d0eb91ac6cc2e9c5c58f5230b1b6f37f0078e
Lais Frigério <laiss@gazetadopovo.com.br>
2021-06-29 00:56:03 -0300

    Add home page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')
+const sass = require('gulp-sass')(require('node-sass'))
 const minify = require('gulp-csso')
 const concat = require('gulp-concat')
 var pug = require('gulp-pug')
@@ -8,12 +8,12 @@ function css() {
   return gulp.src('./resources/styles/app.scss')
     .pipe(sass({utputStyle: 'app', onError: console.error.bind(console, 'Sass error:')}))
     .pipe(minify(concat('app.css')))
-    .pipe(gulp.dest('/css'))
+    .pipe(gulp.dest('./css'))
 }
 
 function templates() {
   return gulp.src([
-    'resources/templates/index.pug',
+    'resources/templates/pages/index.pug',
   ])
   .pipe(pug())
   .pipe(gulp.dest('./'))
