05b8d390ba0229e36c1cfbf2d09a005ae5aafd2f
Lais Frigério <laiss@gazetadopovo.com.br>
2021-06-28 21:52:50 -0300

    Add gulp to compile sass and pug files into css and html respectively

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const minify = require('gulp-csso')
+const concat = require('gulp-concat')
+var pug = require('gulp-pug')
+
+function css() {
+  return gulp.src('./resources/styles/app.scss')
+    .pipe(sass({utputStyle: 'app', onError: console.error.bind(console, 'Sass error:')}))
+    .pipe(minify(concat('app.css')))
+    .pipe(gulp.dest('/css'))
+}
+
+function templates() {
+  return gulp.src([
+    'resources/templates/index.pug',
+  ])
+  .pipe(pug())
+  .pipe(gulp.dest('./'))
+}
+
+exports.css = css
+exports.templates = templates
+exports.default = gulp.parallel(css, templates)
